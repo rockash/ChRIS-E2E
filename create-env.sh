f53df68fb077f2b3fed0109129300f76b5638788
Emilio <i.am.emilio@gmail.com>
2018-01-23 16:49:23 -0500

    adjustments +opnshift install --unstable

--- create-env.sh
+++ create-env.sh
@@ -1,10 +1,38 @@
 #!/usr/bin/bash
 
-oc cluster up
+#install openshift client tools
+sudo dnf install docker
+su -c "echo "\""'INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16"\"" /etc/sysconfig/docker"
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+firewall-cmd --permanent --new-zone dockerc
+firewall-cmd --permanent --zone dockerc --add-source 172.17.0.0/16
+firewall-cmd --permanent --zone dockerc --add-port 8443/tcp
+firewall-cmd --permanent --zone dockerc --add-port 53/udp
+firewall-cmd --permanent --zone dockerc --add-port 8053/udp
+firewall-cmd --reload
+
+wget https://github.com/openshift/origin/releases/download/v3.9.0-alpha.3/openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz.download.zip
+dnf install unzip openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz.download.zip
+unzip openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz.download.zip
+rm -rf __MACOSX/
+mv openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz.download openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz
+tar -xzf openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz/openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit.tar.gz
+sudo cp openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit/oc /usr/bin
+sudo cp openshift-origin-client-tools-v3.9.0-alpha.3-78ddc10-linux-64bit/oc /usr/local/bin
+rm -rf openshift-origin-client-tools*
+
+
+#get pfioh and pman
+git clone https://github.com/FNNDSC/pman.git
+git clone https://github.com/FNNDSC/pfioh.git
+
+sudo oc cluster up
 #Create openshift env
-oc login -u system:admin --insecure-skip-tls-verify=true
-oc create sa robot -n myproject
-oc describe sa robot -n myproject
+sudo oc login -u system:admin --insecure-skip-tls-verify=true
+sudo oc create sa robot -n myproject
+sudo oc describe sa robot -n myproject
 token="$(oc describe sa robot)"
 echo "step 1: '$token'"
 token=$(echo "$token" | grep -A 2 "Mountable secrets" | grep -v "Tokens: ")
@@ -17,8 +45,8 @@ token=$(echo "$token" | tr -d '[:space:]')
 echo "final token: $token"
 
 
-oc adm policy add-role-to-user edit system:serviceaccount:myproject:robot -n myproject
-oc describe secret $token -n myproject
+sudo oc adm policy add-role-to-user edit system:serviceaccount:myproject:robot -n myproject
+sudo oc describe secret $token -n myproject
 token_val=$(oc describe secret $token | grep "token: *" )
 token_val=$(echo "$token_val" | cut -c 7- | sed -e 's/^[ \t]*//')
 echo "Token_val: $token_val"
@@ -27,8 +55,8 @@ echo "Token_val: $token_val"
 #set up shared dir and scc restricted 
 sudo mkdir /tmp/share
 sudo chcon -R -t svirt_sandbox_file_t /tmp/share/
-oc patch scc restricted -p 'allowHostDirVolumePlugin: true'
-oc patch scc restricted -p '"runAsUser": {"type": "RunAsAny"}'
+sudo oc patch scc restricted -p 'allowHostDirVolumePlugin: true'
+sudo oc patch scc restricted -p '"runAsUser": {"type": "RunAsAny"}'
 
 rm -f ~/.kube/config
 oc login --token=$token_val --server=172.30.0.1:443 --insecure-skip-tls-verify=true
@@ -36,8 +64,6 @@ oc project myproject
 oc create secret generic kubecfg --from-file=$HOME/.kube/config -n myproject
 rm -f ~/.kube/config
 oc login --username='developer' --password='developer' --server=localhost:8443 --insecure-skip-tls-verify=true
-git clone https://github.com/FNNDSC/pman.git
-git clone https://github.com/FNNDSC/pfioh.git
 oc new-app pman/openshift/pman-openshift-template-without-swift.json
 oc set env dc/pman OPENSHIFTMGR_PROJECT=myproject
 oc new-app pfioh/openshift/pfioh-openshift-template-without-swift.json
