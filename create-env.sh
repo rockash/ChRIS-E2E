8a44cc691992c53e6bca9c3bd2dd01a64ed18d0f
Emilio <i.am.emilio@gmail.com>
2018-01-24 12:33:52 -0500

    modified to accept debugging args, and install openshift from source

--- create-env.sh
+++ create-env.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/bash
 
+debug=0
+while getopts "dh" opt; do
+    case ${opt} in
+        d )
+            echo "Debugging mode activated"
+            debug=1 ;;
+        h )
+            echo "Passing the -d argument will put this script in debug mode. This add breakpoints to the script that require manual input to unblock, 
+as well as many print statements. Always run this script with sudo permissions." 
+            exit 0;;
+        * )
+            echo "Caught invalid args. Use the -h option to get help.
+Exiting"
+            exit 1 ;;
+    esac
+done
+
 #install openshift client tools
+if [ "$debug" == "1" ]; then
+    printf "\n\nInstall docker from client tools\n\n"
+fi
 sudo dnf install docker
-su -c "echo "\""'INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16"\"" /etc/sysconfig/docker"
+
+if [ "$debug" == "1" ]; then
+    printf "\n\nconfiguring firewall for openshift\n\n"
+fi
+su -c "echo "\""'INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16"\"" >> /etc/sysconfig/docker"
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
@@ -13,34 +37,81 @@ firewall-cmd --permanent --zone dockerc --add-port 53/udp
 firewall-cmd --permanent --zone dockerc --add-port 8053/udp
 firewall-cmd --reload
 
-sudo dnf install origin -y
+if [ "$debug" == "1" ]; then
+    printf "\n\nInstalling openshift origin...\n\n"
+fi
+wget https://github.com/openshift/origin/releases/download/v3.7.1/openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit.tar.gz
+tar -xvf openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit.tar.gz 
+sudo cp sudo cp openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit/oc /usr/bin
+rm openshift-origin-client-tools-v3.7.1-ab0f056-linux-64bit.tar.gz
+
+
+if [ "$debug" == "1" ]; then
+    printf "The next phase installs and attempts to run Chris. Do you want to continue? [y|n]: "
+    read -n 1 cont
+    case "$cont" in
+        y|Y)
+            echo "Running Chris App..." ;;
+        n|N)
+            echo "Exiting!"
+            exit 1 ;;
+        *)
+            echo "Invalid input! Exiting!"
+            exit 1 ;;
+    esac
+fi
+
 
 #get pfioh and pman
+printf "\n\nCloning pman and pfioh\n\n"
 git clone https://github.com/FNNDSC/pman.git
 git clone https://github.com/FNNDSC/pfioh.git
 
 sudo oc cluster up
+
+if [ "$debug" == "1" ]; then
+    printf "creating chris instance in openshift"
+fi
+
 #Create openshift env
 sudo oc login -u system:admin --insecure-skip-tls-verify=true
 sudo oc create sa robot -n myproject
 sudo oc describe sa robot -n myproject
-token="$(oc describe sa robot)"
-echo "step 1: '$token'"
-token=$(echo "$token" | grep -A 2 "Mountable secrets" | grep -v "Tokens: ")
-echo "step 2: $token"
-token=$(echo "$token" | grep "robot-token-*")
-echo "step 3: $token"
-token=$(echo "$token" | cut --delimiter=: --fields=2)
-echo "step 4: $token"
-token=$(echo "$token" | tr -d '[:space:]')
-echo "final token: $token"
 
+
+token="$(oc describe sa robot)"
+if [ "$debug" == "1" ]; then
+    echo "step 1: '$token'"
+fi
+
+token=$(echo "$token" | grep -A 2 "Mountable secrets" | grep -v "Tokens: ")
+if [ "$debug" == "1" ]; then
+    echo "step 2: $token"
+fi
+
+token=$(echo "$token" | grep "robot-token-*")
+if [ "$debug" == "1" ]; then
+    echo "step 3: $token"
+fi
+
+token=$(echo "$token" | cut --delimiter=: --fields=2)
+if [ "$debug" == "1" ]; then
+    echo "step 4: $token"
+fi
+
+token=$(echo "$token" | tr -d '[:space:]')
+if [ "$debug" == "1" ]; then
+    echo "final token: $token"
+fi
 
 sudo oc adm policy add-role-to-user edit system:serviceaccount:myproject:robot -n myproject
 sudo oc describe secret $token -n myproject
 token_val=$(oc describe secret $token | grep "token: *" )
 token_val=$(echo "$token_val" | cut -c 7- | sed -e 's/^[ \t]*//')
-echo "Token_val: $token_val"
+
+if [ "$debug" == "1" ]; then
+    echo "Token_val: $token_val"
+fi
 
 
 #set up shared dir and scc restricted 
@@ -60,6 +131,9 @@ oc set env dc/pman OPENSHIFTMGR_PROJECT=myproject
 oc new-app pfioh/openshift/pfioh-openshift-template-without-swift.json
 
 #install virtual box
+if [ "$debug" == "1" ]; then
+    printf "\n\nInstalling virtual box\n\n"
+fi
 wget http://download.virtualbox.org/virtualbox/5.2.4/VirtualBox-5.2-5.2.4_119785_fedora26-1.x86_64.rpm
 rpm -i VirtualBox-5.2-5.2.4_119785_fedora26-1.x86_64.rpm
 sudo dnf install kernel-devel kernel-devel-4.14.11-300.fc27.x86_64
@@ -67,9 +141,11 @@ sudo dnf install elfutils-libelf-devel
 sudo '/sbin/vboxconfig'
 
 #install vagrant
-wget https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.rpm?_ga=2.103951622.700779187.1515609313-38000827.1515609313
-mv 'vagrant_2.0.1_x86_64.rpm?_ga=2.103951622.700779187.1515609313-38000827.1515609313' vagrant_2.0.1_x86_64.rpm
-rpm -i vagrant_2.0.1_x86_64.rpm
+
+if [ "$debug" == "1" ]; then
+    printf "\n\nInstalling Vagrant\n\n"
+fi
+sudo dnf install vagrant -y
 
 #install vagrant guest package manager
 vagrant plugin install vagrant-vbguest
